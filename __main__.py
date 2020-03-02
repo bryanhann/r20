3804df6ecdd4bd6a900bf4e7f4da6263c0f663d8
Bryan Hann <nobody@nowhere>
2020-03-02 11:43:57 +0800

    Add __exit__ flag handling to r20; beautify.
    Relax the requirement that it be running in a virtual
    environment as well. It is paramount that r20 *works*
    so that the "python -m r20 __exit__ <val>" call will
    prove to the caller that r20 is online for them.
    
    Therefor we want the script to be simple and failsafe.
    We can source out testing for virtual environments to
    other scripts.

--- __main__.py
+++ __main__.py
@@ -1,11 +1,37 @@
-import r20.direct_import as DD
-DD.SAFECALL(__name__)
+#---------------------------------------------------------
+# If we are not imported directly, raise an exception.
 
+if not __name__=='__main__':
+    raise BaseException( 'Must be imported directly' )
 
+#---------------------------------------------------------
+# If our first argument is '__exit__' exit immediately
+# with an error code determined by our second argumentr.
 
-OPT=DD.parser4mip(__package__).parse_args()
+import sys
 
-if OPT.info:
-    DD.STDERR( DD.err_info4mip(__package__))
+if sys.argv[1:2] == [ '__exit__' ]:
+    try:
+        err=int(sys.argv[2])
+    except:
+        err=-1
+    exit(err)
+
+#---------------------------------------------------------
+# Consult argparse and act acordingly.
+
+import r20.direct_import as DIRECT
+
+mip=__package__
+
+info_msg = DIRECT.err_info4mip(mip)
+help_msg = DIRECT.err_try4mip(mip)
+parser   = DIRECT.parser4mip(mip)
+write    = DIRECT.STDERR
+
+o=parser.parse_args()
+
+if o.info:
+    write( info_msg )
 else:
-    DD.STDERR( DD.err_try4mip(__package__) )
+    write( help_msg )
